5084e6917da028187c843afd9df7bad2847a63ef
App Academy Student <appacademy@Apps-Mac-mini-11.local>
2016-05-31 16:52:16 -0400

    disable seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-User.destroy_all
-victor = User.create(username: "Victor", password: "password")
-vincent = User.create(username: "Vincent", password: "password")
-vanessa = User.create(username: "Vanessa", password: "password")
-vincenzo = User.create(username: "Vincenzo", password: "password")
-victoria = User.create(username: "Victoria", password: "password")
-valentino = User.create(username: "Valentino", password: "password")
-vlad = User.create(username: "Vlad@", password: "password")
-vladimir = User.create(username: "Vladimir", password: "password")
+# User.destroy_all
+# victor = User.create(username: "Victor", password: "password")
+# vincent = User.create(username: "Vincent", password: "password")
+# vanessa = User.create(username: "Vanessa", password: "password")
+# vincenzo = User.create(username: "Vincenzo", password: "password")
+# victoria = User.create(username: "Victoria", password: "password")
+# valentino = User.create(username: "Valentino", password: "password")
+# vlad = User.create(username: "Vlad@", password: "password")
+# vladimir = User.create(username: "Vladimir", password: "password")
