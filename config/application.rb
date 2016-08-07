5797ae8f510c5edee8c56a78c2510146c69b2274
victoracechen <victoracechen@gmail.com>
2016-08-07 13:51:38 -0400

    update aws credentials

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module OddJob
     # -- all .rb files in that directory are automatically loaded.
     config.paperclip_defaults = {
       :storage => :s3,
+      :s3_region => ENV["s3_region"],
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
